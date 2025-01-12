683fafa823b14670fd26da3f32b598214ece4cd0
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-12 22:24:46 +0530

    changes

---
+++ date.js
@@ -0,0 +1,17 @@
+//  dates 
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString() );
+// console.log(typeof myDate);
+
+
+let myCreatedDate = new Date (2025 , 0 , 25)
+console.log(myCreatedDate.toDateString());
+
+
+
