a602cb3d7956856713b3a6c2351545b47727c045
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-12 22:32:55 +0530

    changes

--- date.js
+++ date.js
@@ -10,8 +10,25 @@ let myDate = new Date()
 // console.log(typeof myDate);
 
 
-let myCreatedDate = new Date (2025 , 0 , 25)
-console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date (2025 , 0 , 25)
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date ("2025-01-13")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
+
+
+
+ 
 
 
 
