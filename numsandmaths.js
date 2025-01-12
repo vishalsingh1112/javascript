81e50178470ccbc3429de8df182b624637d024b4
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-12 22:15:36 +0530

    maths and numbers

---
+++ numsandmaths.js
@@ -0,0 +1,50 @@
+const score= 4000
+// console.log(score);
+
+
+const balance = new Number (100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const othernumber = 23.456455
+// console.log(othernumber.toFixed(4));
+// console.log(othernumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString(`en-In`));
+
+ 
+//  ************ MATHS ********
+
+console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(7.8));
+// console.log(Math.ceil(9.8));
+// console.log(Math.floor(8.3));
+// console.log(Math.min(4 , 5,6,7,8));
+// console.log(Math.max(4,5,6,7,8,9));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
