9b1741d6883c740029fe4fd1b3fad2c19f49ef3d
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-13 10:35:06 +0530

    changes

--- java basics 02/arrays.js
+++ java basics 02/arrays.js
@@ -1,2 +1,58 @@
 // arrays 
 
+ const myArr =[0 , 1 ,2 ,3,4,5]
+// const myHeros = ["thor" , "spiderman" , "iron-man"]
+
+// const myArr2 = new Array(1 ,2 , 3 , 4 ,5)
+// console.log(myHeros[2]);
+// console.log(myArr[3]);
+// console.log(myArr2[2]);
+
+//  array methods 
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(1));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(  newArr);
+// console.log(myArr);
+
+
+//    slice  , splice 
+
+console.log("A " , myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B " ,myArr);
+
+const myn2 = myArr.splice(1 ,3 )
+console.log(myn2);
+console.log("C " , myArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ java basics 02/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
