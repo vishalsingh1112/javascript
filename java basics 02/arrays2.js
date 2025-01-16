4c1d181e8f0269cea33eb60813a46ef90311c32e
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-16 22:29:32 +0530

    changes

--- java basics 02/arrays2.js
+++ java basics 02/arrays2.js
@@ -20,5 +20,16 @@ const another_array=[ 1 , 2 ,3 ,4 ,[5 ,6,7], [6,7,[9,8,7]]]
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array );
 
+console.log( Array.isArray("vishal"));
+console.log( Array.from("vishal"));
+console.log( Array.from({name: "vishal"}));
+
+let score1= 100
+let score2 = 300
+let score3 = 200
+
+console.log(Array.of(score1 , score2,score3));
+
+  
  
  
