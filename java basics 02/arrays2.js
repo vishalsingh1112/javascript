aa9fcbc5f116b39a6d4fb16b67db55e6b838eba6
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-16 22:17:43 +0530

    changes

--- java basics 02/arrays2.js
+++ java basics 02/arrays2.js
@@ -2,9 +2,15 @@ const marvel_heros = [ "thor" , "ironman" ,"vision"]
 const dc_heros = ["superman" , "flash" ,"batman"] 
 
  
- marvel_heros.push(dc_heros)
- console.log(marvel_heros);
+//   marvel_heros.push(dc_heros)
+//  console.log(marvel_heros);
 
- console.log(marvel_heros[3] [0]);
+//  console.log(marvel_heros[3] [0]);
+
+
+//   const allHeros = marvel_heros.concat(dc_heros)
+//  console.log(allHeros );
+
+
  
  
