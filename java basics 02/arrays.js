ab8dd6a94053cd2ee572a1dbf78045d66dcfc892
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-16 22:13:53 +0530

    changes

--- java basics 02/arrays.js
+++ java basics 02/arrays.js
@@ -40,6 +40,8 @@ const myn2 = myArr.splice(1 ,3 )
 console.log(myn2);
 console.log("C " , myArr);
 
+  
+
 
 
 
---
+++ java basics 02/arrays2.js
@@ -0,0 +1,10 @@
+const marvel_heros = [ "thor" , "ironman" ,"vision"]
+const dc_heros = ["superman" , "flash" ,"batman"] 
+
+ 
+ marvel_heros.push(dc_heros)
+ console.log(marvel_heros);
+
+ console.log(marvel_heros[3] [0]);
+ 
+ 
