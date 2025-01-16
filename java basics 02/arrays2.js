dec4625492407d519295d02f306283e3e88c8e66
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-16 22:24:32 +0530

    changes

--- java basics 02/arrays2.js
+++ java basics 02/arrays2.js
@@ -11,6 +11,14 @@ const dc_heros = ["superman" , "flash" ,"batman"]
 //   const allHeros = marvel_heros.concat(dc_heros)
 //  console.log(allHeros );
 
+// const all_new_heros= [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros);
+
+
+const another_array=[ 1 , 2 ,3 ,4 ,[5 ,6,7], [6,7,[9,8,7]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array );
 
  
  
