fe1892477330e88bd157efdc494895fe7d27cf32
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-29 21:31:17 +0530

    changes

---
+++ 04_logic control/01_.js
@@ -0,0 +1,59 @@
+//  if 
+
+const isUserloggedIn = true
+const temperature =43
+
+// if(temperature === 43 ){
+//     console.log("temperature is les than 50");
+//     }else{
+//     console.log("temperature is greater than 50 ");
+//     }
+//     console.log("executed ");
+    
+
+// const score = 200;
+// if (score>100) {
+//    const power ="fly"
+//     console.log(`user power: ${power}`);
+    
+    
+// }
+// console.log(`user power: ${power}`);
+    
+
+// const balance =1000;
+
+// if (balance >500) console.log("test"),console.log("test 2");  not to use 
+
+// if (balance <500) {
+// console.log("less than");
+
+// }else if (balance<750){
+//     console.log("less than 750");
+    
+// }else if (balance <900){
+//     console.log("less than 900");
+    
+// }else {
+//     console.log("less than 1200");
+// }
+    
+
+const userloggedIn = true
+const debitcard = true
+const loggedInFromGoggle = false
+const loggedInFromEmail = true
+
+if (userloggedIn && debitcard && 2==3) {
+    console.log("allow to buy courses");
+    
+}
+if (loggedInFromGoggle || loggedInFromEmail) {
+    console.log("user logged in");
+    
+    
+}
+
+
+
+
