e3df41b69e0c72c8eff5862d2fc1fe21e8c09295
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-29 22:49:53 +0530

    changes

---
+++ 04_logic control/switches.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month  = "april"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("febuary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    case "june":
+        console.log("june");
+        break;
+    case "july":
+        console.log("july");
+        break;
+    case "aug":
+        console.log("august");
+        break;
+    case "sep":
+        console.log("september");
+        break;
+    case "oct":
+        console.log("october");
+        break;
+    case "nov":
+        console.log("november");
+        break;
+    case "dec":
+        console.log("december");
+        break;
+
+    default:
+        console.log("not find ");
+        
+        break;
+}
---
+++ 04_logic control/truthy.js
@@ -0,0 +1,51 @@
+const userEmail =[]
+// if (userEmail) {
+//     // console.log("got user email");
+    
+// }
+// else{
+//     // console.log("dont have user email");
+    
+// }
+
+// falsy values 
+
+// false , 0 ,-0 , BigInt 0n , "" ,null , undefined , NaN
+
+//  truthy values 
+
+//  "0", 'false', " " , [] , {}, function(){} 
+
+// if (userEmail.length===0) {
+//     console.log("array is empty");
+// }
+
+// const emptyobj ={}
+// if (Object.keys(emptyobj).length===0) {÷
+    //   console.log("object is empty");
+      
+// }
+
+//  Nullish coalescing objects (??): null , undefined 
+
+// let val1;
+// // val1= 5 ?? 10
+// // val1 = null ?? 10
+// // val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 15
+// console.log(val1);
+
+
+//  Terniary operator
+
+// condition ? tru : false
+
+const iceTeaPrice= 100
+iceTeaPrice<=80 ? console.log("less than 80"): console.log("more than 80");
+
+
+
+
+
+
+ 
