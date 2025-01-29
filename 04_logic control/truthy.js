b222aac66bc5d87be260eb02a1a6b8d453cc433f
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-29 23:00:02 +0530

    changes

--- 04_logic control/truthy.js
+++ 04_logic control/truthy.js
@@ -38,7 +38,7 @@ const userEmail =[]
 
 //  Terniary operator
 
-// condition ? tru : false
+// condition ? true : false
 
 const iceTeaPrice= 100
 iceTeaPrice<=80 ? console.log("less than 80"): console.log("more than 80");
