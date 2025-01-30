bf4ac6819abbb296291ccbe8c180f83deeb9d06c
vishalsingh1112 <vishalsavitasingh@gmail.com>
2025-01-30 11:32:13 +0530

    changes

---
+++ 04_logic control/while&dowhile.js
@@ -0,0 +1,26 @@
+let index =0
+while (index <=10) {
+// console.log(`value of index is ${index}`);
+ index = index+2
+    
+}
+
+
+let myArray = ["flash" , "superman" , "batman"]
+
+let arr = 0
+while (arr < myArray.length) {
+// console.log(`value is ${myArray[arr]}`);
+arr =arr+1
+    
+}
+
+
+//  do while loop
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10);
